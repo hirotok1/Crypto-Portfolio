f231d4222135105fb63ca7d1b8b50b5e5ec92d16
hirotok1 <trophymax1@yahoo.co.jp>
2025-02-04 04:24:08 +0000

    auth middleware added to transaction

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,17 @@ Route::get('/market', [MarketController::class, 'index'])->name('market');
 
 Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');
 
-Route::get('/transaction/index', [TransactionController::class, 'index'])->name('transaction.index');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/transaction/index', [TransactionController::class, 'index'])->name('transaction.index');
+    Route::get('/transaction/create', [TransactionController::class, 'create'])->name('transaction.create');
+    Route::post('/transaction/swap', [TransactionController::class, 'storeSwap'])->name('transaction.storeSwap');
+    Route::post('/transaction/send', [TransactionController::class, 'storeSend'])->name('transaction.storeSend');
+    Route::post('/transaction/deposit', [TransactionController::class, 'storeDeposit'])->name('transaction.storeDeposit');
 
-Route::get('/transaction/create', [TransactionController::class, 'create'])->name('transaction.create');
-Route::post('/transaction/swap', [TransactionController::class, 'storeSwap'])->name('transaction.storeSwap');
-Route::post('/transaction/send', [TransactionController::class, 'storeSend'])->name('transaction.storeSend');
-Route::post('/transaction/deposit', [TransactionController::class, 'storeDeposit'])->name('transaction.storeDeposit');
-
-Route::delete('/transaction/delete-swap/{swap}', [TransactionController::class, 'deleteSwap'])->name('transaction.deleteSwap');
-Route::delete('/transaction/delete-send/{send}', [TransactionController::class, 'deleteSend'])->name('transaction.deleteSend');
-Route::delete('/transaction/delete-deposit/{deposit}', [TransactionController::class, 'deleteDeposit'])->name('transaction.deleteDeposit');
+    Route::delete('/transaction/delete-swap/{swap}', [TransactionController::class, 'deleteSwap'])->name('transaction.deleteSwap');
+    Route::delete('/transaction/delete-send/{send}', [TransactionController::class, 'deleteSend'])->name('transaction.deleteSend');
+    Route::delete('/transaction/delete-deposit/{deposit}', [TransactionController::class, 'deleteDeposit'])->name('transaction.deleteDeposit');
+});
 
 Route::get('/set-currency/{currency}', function ($currency) {
     session(['currency' => $currency]);
